1f39685d1aeffa5db89cab2bba03cbf1e50685f7
Adam Roesner <aaroesner@gmail.com>
2025-04-30 19:27:15 -0400

    Add support for file reading

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 use anyhow::Result;
 use base64::{prelude::BASE64_STANDARD, Engine};
 use clap::{ArgAction, Parser};
+use clap_stdin::FileOrStdin;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    input: String,
+    input: FileOrStdin,
     #[arg(short, long, default_value = "std_out")]
     out: String,
     #[arg(short, long, action=ArgAction::SetTrue)]
     decode: bool,
     #[arg(short, long, action=ArgAction::SetTrue)]
-    json: bool
-}
-
-impl Args {
-    fn format_string(&self) -> String {
-        format!("input: {}\nout: {}\ndecode: {}\njson: {}\n", self.input, self.out, self.decode, self.json)
-    }
+    json: bool,
+    #[arg(long, action=ArgAction::SetTrue)]
+    debug: bool,
 }
 
 enum CustomError {
@@ -28,30 +25,40 @@ enum CustomError {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    print!("{}", args.format_string());
+    if args.debug { println!("{:?}", args); }
 
     if args.decode && args.json {
         panic!("Invalid arguments")
     }
 
-    let decoded = match args.decode {
-        true => decode_in(args.input)?,
-        false => encode_in(args.input)?
+    let input = args.input.contents()?;
+
+    let input = match args.json {
+        true => parse_json(input)?,
+        false => input
     };
 
-    println!("{}", decoded);
-    println!("{:?}", String::from_utf8(BASE64_STANDARD.decode("dGV4dA==")?).unwrap());
+    let decoded = match args.decode {
+        true => decode_in(input)?,
+        false => encode_in(input)?
+    };
+
+    print!("{}", decoded);
 
     Ok(())
 }
 
 fn decode_in(inp: String) -> Result<String> {
-    println!("decoding {}", inp);
-    let decoded = BASE64_STANDARD.decode(inp.as_bytes())?;
+    let decoded = BASE64_STANDARD.decode(inp)?;
     let decoded = String::from_utf8(decoded)?;
     Ok(decoded)
 }
 fn encode_in(inp: String) -> Result<String> {
     let decoded = BASE64_STANDARD.encode(inp);
     Ok(decoded)
+}
+fn parse_json(inp: String) -> Result<String> {
+    println!("{}", inp);
+
+    Ok(inp)
 }
