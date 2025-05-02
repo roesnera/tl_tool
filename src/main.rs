d890a8fa9ddee7684cc17a3d4bdca88438d2b385
Adam Roesner <aaroesner@gmail.com>
2025-05-01 20:07:58 -0400

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use anyhow::Result;
 use base64::{prelude::BASE64_STANDARD, Engine};
 use clap::{ArgAction, Parser};
@@ -17,25 +17,20 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    if args.debug { println!("{:?}", args); }
-
     let mut input = String::new();
 
     if let Some(content) = args.input {
         input = content;
     } else {
-        println!("else condition reached");
         let _ = std::io::stdin().read_to_string(&mut input);
     }
-
-    println!("{}", input);
 
     let decoded = match args.decode {
         true => decode_in(input)?,
         false => encode_in(input)?
     };
 
-    print!("{}", decoded);
+    let _ = std::io::stdout().write(decoded.as_bytes());
 
     Ok(())
 }
@@ -46,6 +41,35 @@ fn decode_in(inp: String) -> Result<String> {
     Ok(decoded)
 }
 fn encode_in(inp: String) -> Result<String> {
-    let decoded = BASE64_STANDARD.encode(inp);
+    let decoded = BASE64_STANDARD.encode(inp.as_bytes());
     Ok(decoded)
+}
+
+#[test]
+fn decode_str_test() {
+    let input = String::from("aGVxbG8=");
+    let decoded = decode_in(input);
+
+    assert!(decoded.is_ok());
+}
+
+#[test]
+fn encode_str_test() {
+    let input = String::from("hello");
+
+    assert!(encode_in(input).is_ok());
+}
+
+#[test]
+fn bidirectional() {
+    let original = String::from("hello");
+    let encoded = encode_in(original);
+    assert!(encoded.is_ok());
+
+    let encoded = encoded.unwrap();
+    let decoded = decode_in(encoded);
+    assert!(decoded.is_ok());
+
+    let decoded = decoded.unwrap();
+    assert_eq!(decoded, "hello");
 }
